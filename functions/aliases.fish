f47dccd74948125dcfac657157dd5ceee7afd931
Chad Seeger <cseeger@truecar.com>
2017-03-07 15:11:20 -0800

    rails aliases

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -1,5 +1,6 @@
 alias l="ls -las"
 
+# Git
 alias g="git"
 alias grhh="git reset HEAD --hard"
 alias gd="git diff"
@@ -10,3 +11,8 @@ alias gpom="git pull origin master"
 # Quick check running state of a process
 alias psef="ps -ef | peco "
 alias top="top -o cpu"
+
+# Rails
+alias be="bundle exec"
+alias rs="bundle exec rails s"
+alias rc="bundle exec rails c"
