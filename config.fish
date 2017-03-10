fe88c7b7c08ca53cb56f55e3849f3da78154232b
Chad Seeger <cseeger@truecar.com>
2017-03-09 18:04:21 -0800

    wire up autojump source on init

--- config.fish
+++ config.fish
@@ -1,1 +1,8 @@
 . "$HOME/.config/fish/functions/aliases.fish"
+
+begin
+  set --local AUTOJUMP_PATH $HOME/.config/fish/functions/autojump.fish
+  if test -e $AUTOJUMP_PATH
+      source $AUTOJUMP_PATH
+  end
+end
