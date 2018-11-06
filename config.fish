3fbeb00e8f160042add1a3b48d3ade99f43cc4f9
Chad Seeger <cseeger@users.noreply.github.com>
2018-11-05 17:04:35 -0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,5 @@
 . "$HOME/.config/fish/functions/aliases.fish"
 
-begin
-  set --local AUTOJUMP_PATH $HOME/.config/fish/functions/autojump.fish
-  if test -e $AUTOJUMP_PATH
-      source $AUTOJUMP_PATH
-  end
-end
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 status --is-interactive; and source (rbenv init -|psub)
